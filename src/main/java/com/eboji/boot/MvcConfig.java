f1f04c6b551574e69b5048c11cccb8d9e0e535f7
zhoucailiang <zhoucailiang@shijie99.com>
2016-05-18 20:28:28 +0800

    fastjson替换spring默认json

--- src/main/java/com/eboji/boot/MvcConfig.java
+++ src/main/java/com/eboji/boot/MvcConfig.java
@@ -1,20 +1,24 @@
 package com.eboji.boot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 @Configuration
 @EnableWebMvc
@@ -48,8 +52,22 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 			List<HttpMessageConverter<?>> converters) {
 		StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
         stringConverter.setWriteAcceptCharset(false);
-        converters.add(new MappingJackson2HttpMessageConverter());
         
+        FastJsonHttpMessageConverter jsonConverter = new FastJsonHttpMessageConverter();
+        List<MediaType> jsonMediaTypes = new ArrayList<MediaType>();
+        jsonMediaTypes.add(MediaType.valueOf(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"));
+        jsonMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        jsonConverter.setSupportedMediaTypes(jsonMediaTypes);
+        
+        Jaxb2RootElementHttpMessageConverter xmlConverter = new Jaxb2RootElementHttpMessageConverter();
+        List<MediaType> xmlMediaTypes = new ArrayList<MediaType>();
+        xmlMediaTypes.add(MediaType.valueOf(MediaType.TEXT_XML_VALUE + ";charset=UTF-8"));
+        xmlMediaTypes.add(MediaType.valueOf(MediaType.APPLICATION_XML_VALUE + ";charset=UTF-8"));
+        xmlConverter.setSupportedMediaTypes(xmlMediaTypes);
+        
+        converters.add(stringConverter);
+        converters.add(jsonConverter);
+        converters.add(xmlConverter);
 		super.configureMessageConverters(converters);
 	}
 	
